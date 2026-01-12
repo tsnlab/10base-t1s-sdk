507f6393ee81dfb76bf4dfcd3dc935f992b2d560
pak-ji <pakji@tsnlab.com>
2026-01-12 11:51:54 +0900

    Remove NOLINT comments

--- src/kernel/lan865x/lan865x.c
+++ src/kernel/lan865x/lan865x.c
@@ -6,7 +6,7 @@
  * Modified: sbcho + ChatGPT integration
  */
 
-#include <linux/device.h> // NOLINT
+#include <linux/device.h>
 #include <linux/fs.h>
 #include <linux/kernel.h>
 #include <linux/miscdevice.h>
