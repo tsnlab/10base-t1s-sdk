8120f10e835b809b93c5aaf9e06e75e0fd553343
pak-ji <pakji@tsnlab.com>
2026-02-24 11:26:47 +0900

    Remove LAN865x TSH register usage code

--- src/kernel/lan865x/lan865x_arch.c
+++ src/kernel/lan865x/lan865x_arch.c
@@ -15,22 +15,16 @@ sysclock_t lan865x_get_sys_clock(struct lan865x_priv* priv) {
     struct oa_tc6* tc6 = priv->tc6;
     int ret;
 
-    u32 sec_h;
-    u32 sec_l;
+    u32 sec;
     u32 nsec;
     u64 tmp_sec, clock;
 
     if ((ret = oa_tc6_read_register(tc6, MMS1_MAC_TN, &nsec)))
         return ret;
-    if ((ret = oa_tc6_read_register(tc6, MMS1_MAC_TSL, &sec_l)))
+    if ((ret = oa_tc6_read_register(tc6, MMS1_MAC_TSL, &sec)))
         return ret;
-
-    /* NOTE: Linux timespec64 is 64-bit, but we only use 32-bit seconds and nanoseconds */
-    //if (oa_tc6_read_register(tc6, MMS1_MAC_TSH, &sec_h))
-    //    return ret;
-    (void)sec_h;
         
-    tmp_sec = (u64)sec_l * NS_IN_1S;
+    tmp_sec = (u64)sec * NS_IN_1S;
     nsec = nsec & 0x3FFFFFFF;
 
     clock = tmp_sec + nsec;
@@ -42,24 +36,18 @@ sysclock_t lan865x_get_sys_clock_and_delay(struct lan865x_priv* priv, struct ptp
     struct oa_tc6* tc6 = priv->tc6;
     int ret;
 
-    u32 sec_h;
-    u32 sec_l;
+    u32 sec;
     u32 nsec;
     u64 tmp_sec, clock;
 
     ptp_read_system_prets(res_spi_delay);
     if ((ret = oa_tc6_read_register(tc6, MMS1_MAC_TN, &nsec)))
         return ret;
-    if ((ret = oa_tc6_read_register(tc6, MMS1_MAC_TSL, &sec_l)))
+    if ((ret = oa_tc6_read_register(tc6, MMS1_MAC_TSL, &sec)))
         return ret;
     ptp_read_system_postts(res_spi_delay);
 
-    /* NOTE: Linux timespec64 is 64-bit, but we only use 32-bit seconds and nanoseconds */
-    //if (oa_tc6_read_register(tc6, MMS1_MAC_TSH, &sec_h))
-    //    return ret;
-    (void)sec_h;
-
-    tmp_sec = (u64)sec_l * NS_IN_1S;
+    tmp_sec = (u64)sec * NS_IN_1S;
     nsec = nsec & 0x3FFFFFFF;
 
     clock = tmp_sec + nsec;
