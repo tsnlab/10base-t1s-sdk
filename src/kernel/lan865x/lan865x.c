cf297c7edbb729f9c4ee528313d13846fae45a37
pak-ji <pakji@tsnlab.com>
2026-02-25 11:26:46 +0900

    Refactor separate ethtool support from lan865x.c

--- src/kernel/lan865x/lan865x.c
+++ src/kernel/lan865x/lan865x.c
@@ -24,6 +24,7 @@
 
 #include "lan865x_ioctl.h"
 #include "lan865x_arch.h"
+#include "lan865x_ethtool.h"
 #include "lan865x_sysfs.h"
 #include "t1s_hat_fxl6408.h"
 #ifdef FRAME_TIMESTAMP_ENABLE
@@ -139,42 +140,6 @@ static int lan865x_set_hw_macaddr(struct lan865x_priv *priv, const u8 *mac)
 
     return ret;
 }
-
-#ifdef FRAME_TIMESTAMP_ENABLE
-static int lan865x_ethtool_get_ts_info(struct net_device* netdev, struct ethtool_ts_info* ts_info) {
-    struct lan865x_priv* priv = (struct lan865x_priv*)netdev_priv(netdev);
-
-    if (!priv->ptpdev) {
-        return -EOPNOTSUPP;
-    }
-
-    ts_info->phc_index = ptp_clock_index(priv->ptpdev->ptp_clock);
-
-    ts_info->so_timestamping = SOF_TIMESTAMPING_TX_SOFTWARE | SOF_TIMESTAMPING_RX_SOFTWARE | SOF_TIMESTAMPING_SOFTWARE |
-                               SOF_TIMESTAMPING_TX_HARDWARE | SOF_TIMESTAMPING_RX_HARDWARE |
-                               SOF_TIMESTAMPING_RAW_HARDWARE;
-
-    ts_info->tx_types = BIT(HWTSTAMP_TX_OFF) | BIT(HWTSTAMP_TX_ON);
-
-    ts_info->rx_filters = BIT(HWTSTAMP_FILTER_NONE) | BIT(HWTSTAMP_FILTER_ALL) | BIT(HWTSTAMP_FILTER_PTP_V2_L2_EVENT) |
-                          BIT(HWTSTAMP_FILTER_PTP_V2_L2_SYNC) | BIT(HWTSTAMP_FILTER_PTP_V2_L2_DELAY_REQ);
-
-    return 0;
-}
-#else /* FRAME_TIMESTAMP_ENABLE */
-static int lan865x_ethtool_get_ts_info(struct net_device* netdev, struct ethtool_ts_info* ts_info) {
-    return -EOPNOTSUPP;
-}
-#endif /* FRAME_TIMESTAMP_ENABLE */
-
-static const struct ethtool_ops lan865x_ethtool_ops = {
-    .get_link_ksettings = phy_ethtool_get_link_ksettings,
-    .set_link_ksettings = phy_ethtool_set_link_ksettings,
-#ifdef FRAME_TIMESTAMP_ENABLE
-    .get_ts_info = lan865x_ethtool_get_ts_info,
-#endif /* FRAME_TIMESTAMP_ENABLE */
-};
-
 
 static int lan865x_set_mac_address(struct net_device *netdev, void *addr)
 {
---
+++ src/kernel/lan865x/lan865x_ethtool.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * LAN865x ethtool operations header
+ *
+ * Microchip's LAN865x 10BASE-T1S MAC-PHY driver
+ */
+
+#ifndef LAN865X_ETHTOOL_H
+#define LAN865X_ETHTOOL_H
+
+#include <linux/ethtool.h>
+
+extern const struct ethtool_ops lan865x_ethtool_ops;
+
+#endif /* LAN865X_ETHTOOL_H */
